38ec6b99188766e05df0725708236b8dd855a291
Yousif Maqdasi <Yousif.maqdasi@chasacademy.se>
2026-01-30 14:43:15 +0100

    added stuff

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -3,40 +3,13 @@
 
 import './App.css';
 import RecipeCard from './components/RecipeCard';
-import { useState } from 'react';
 
-type Recipe = {
-  title: string;
-  image: string;
-  have: string[];
-  need: string[];
-  tag: string;
-  tagColor?: string;
-}
 
 function App() {
 
-  const [budget, setBudget] = useState("Under $5");
-  const [difficulty, setDifficulty] = useState("Easy");
-  const [ingredients, setIngredients] = useState(""); ``
-  const [recipes, setRecipes] = useState<Recipe[]>([]);
-
   return (
     <>
-    <RecipeCard 
-    title='pizza' 
-    image='' 
-    have={["Pepperoni", "Cheese"]} 
-    need={["Flour", "Yeast"]}
-    tag='Cheap And Quick!' />
-    
-    <RecipeCard 
-    title='burger' 
-    image='' 
-    have={["Buns", "Cheese"]} 
-    need={["Beef Patty", "Lettuce", "Tomato"]} 
-    tag='Fast Food' />
-    </>
+    <RecipeCard title='pizza' image='' have={["Pepperoni", "Cheese"]} need={["Flour", "Yeast"]}tag='Cheap And Quick!' /></>
   )
 }
 
